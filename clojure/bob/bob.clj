2aea6c928dc1e20aa03abe070fedc063ccdf9139
Eugene Hlyzov <hlyzov@gmail.com>
2014-04-18 09:30:22 +0400

    [clojure][bob] new version

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,28 +1,30 @@
 (ns bob
   (:require [clojure.string :refer [upper-case trim]]))
 
-(defn- classify [msg]
-  (cond
-   (and (re-find #"\p{Alpha}" msg) 
-        (= (upper-case msg) msg)) 
-   :loud
+(def ^:private properties-map
+  (hash-map
+   :question   #(= \? (last %))
+   :letters    #(re-find #"\p{Alpha}" %)
+   :stress     #(= (upper-case %) %)
+   :empty      #(= "" %)))
+   
 
-   (= \? (last msg)) 
-   :question
-
-   (= "" msg) 
-   :nothing
-
-   :else :normal)
-  )
-
-(def ^:private msg-map 
-  {
+(def ^:private reaction-map 
+  (hash-map
+   :yelling "Woah, chill out!"
+   :asking "Sure."
    :nothing "Fine. Be that way!"
-   :loud "Woah, chill out!"
-   :question "Sure."
-   :normal "Whatever."
-   })
+   :something "Whatever."))
+
+(defn- guess-intention [properties]
+  (condp (fn [a b] (every? b a)) properties
+   #{:stress :letters} :yelling
+   #{:question} :asking
+   #{:empty} :nothing
+   :something))
+
+(defn- collect-properties [msg]
+  (set (keys (filter #((last %) msg) properties-map))))
 
 (defn response-for [msg]
-  (msg-map (classify (trim msg))))
+  (reaction-map (guess-intention (collect-properties (trim msg)))))
